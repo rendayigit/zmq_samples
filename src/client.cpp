c337d4cc650601ffcac791b589ddf39c3cda70a2
rendayigit <renda.yigit@gmail.com>
2024-12-13 16:22:27 +0300

    minor refactoring

--- src/client.cpp
+++ src/client.cpp
@@ -4,36 +4,41 @@
 #include <zmq.hpp>
 #include <zmq_addon.hpp>
 
+const std::string HOST = "localhost";
+const std::string PORT = "5555";
+
+constexpr int THREAD_SLEEP_DURATION = 100;
+
 int main() {
   // Create a context
   zmq::context_t context(1);
 
   // Create a socket of type REQ
   zmq::socket_t subscriber(context, zmq::socket_type::sub);
-  subscriber.connect("tcp://localhost:5555"); // Connect to the server
 
-  // Subscribe to A envelope
+  // Connect to the server
+  subscriber.connect("tcp://" + HOST + ":" + PORT);
+
+  // Subscribe to A topic
   subscriber.set(zmq::sockopt::subscribe, "A");
 
-  // Subscribe to TERMINAL envelope
+  // Subscribe to TERMINAL topic
   subscriber.set(zmq::sockopt::subscribe, "TERMINAL");
 
-  // Subscribe to JSON envelope
+  // Subscribe to JSON topic
   subscriber.set(zmq::sockopt::subscribe, "JSON");
 
   while (true) {
     // Receive all parts of the message
     std::vector<zmq::message_t> recvMsgs;
     zmq::recv_result_t result = zmq::recv_multipart(subscriber, std::back_inserter(recvMsgs));
-    assert(result && "recv failed");
+    assert(result && "\n>>> recv failed");
     assert(*result == 2);
 
-    std::cout << "Client received: " << std::endl;
-    for (auto &message : recvMsgs) {
-      std::cout << message.to_string() << std::endl;
-    }
+    std::cout << "\n>>> client received: " << std::endl;
+    std::cout << ">>> TOPIC: " << recvMsgs.at(0).to_string() << ", MESSAGE: " << recvMsgs.at(1).to_string() << std::endl;
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(THREAD_SLEEP_DURATION));
   }
 
   return 0;
--- src/server.cpp
+++ src/server.cpp
@@ -6,6 +6,10 @@
 #include <thread>
 #include <zmq.hpp>
 
+constexpr int THREAD_SLEEP_DURATION = 1000;
+
+const std::string PORT = "5555";
+
 static zmq::context_t context(1);
 static zmq::socket_t publisher(context, zmq::socket_type::pub);
 static unsigned long long counter = 0;
@@ -16,21 +20,21 @@ void publisherThread() {
     try {
       // Simple publish
       publisher.send(zmq::str_buffer("A"), zmq::send_flags::sndmore);
-      publisher.send(zmq::str_buffer("Message in A envelope"));
+      publisher.send(zmq::str_buffer("message in A topic"));
 
       publisher.send(zmq::str_buffer("A"), zmq::send_flags::sndmore);
-      publisher.send(zmq::str_buffer("More A messages"));
+      publisher.send(zmq::str_buffer("more A messages"));
 
       publisher.send(zmq::str_buffer("B"), zmq::send_flags::sndmore);
-      publisher.send(zmq::str_buffer("Message in B envelope"));
+      publisher.send(zmq::str_buffer("message in B topic"));
 
       publisher.send(zmq::str_buffer("C"), zmq::send_flags::sndmore);
-      publisher.send(zmq::str_buffer("Message in C envelope"));
+      publisher.send(zmq::str_buffer("message in C topic"));
 
       // Publish a JSON message
       publisher.send(zmq::str_buffer("JSON"), zmq::send_flags::sndmore);
       nlohmann::json request;
-      request["a"] = 123;
+      request["a"] = 1;
       std::string jsonStr = request.dump();
       zmq::message_t jsonMessage(jsonStr.data(), jsonStr.size());
       publisher.send(jsonMessage, zmq::send_flags::none);
@@ -38,15 +42,19 @@ void publisherThread() {
       // Publish terminal standard outputs
       if (not local.str().empty()) {
         publisher.send(zmq::str_buffer("TERMINAL"), zmq::send_flags::sndmore);
+        
         zmq::message_t message(local.str().data(), local.str().size());
         publisher.send(message, zmq::send_flags::none);
+        
+        // Clear terminal stream
+        local.str("");
         local.clear();
       }
-
-      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     } catch (std::exception const &e) {
       continue; // Waiting for a connection
     }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(THREAD_SLEEP_DURATION));
   }
 }
 
@@ -54,16 +62,18 @@ void counterThread() {
   while (true) {
     // Populate terminal standard outputs
     std::cout << counter++ << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    std::this_thread::sleep_for(std::chrono::milliseconds(THREAD_SLEEP_DURATION));
   }
 }
 
 int main() {
-  publisher.bind("tcp://*:5555"); // Bind to port 5555
+  // Bind to port 5555
+  publisher.bind("tcp://*:" + PORT);
 
-  std::cout << "Server is running on port 5555..." << std::endl;
+  std::cout << ">>> server is running on port " << PORT << std::endl;
 
-  std::cout.rdbuf(local.rdbuf()); // Forward cout to local string stream
+  // Forward cout to local string stream
+  std::cout.rdbuf(local.rdbuf());
 
   std::thread counterThreadObj([&] { counterThread(); });
   std::thread publisherThreadObj([&]() { publisherThread(); });
